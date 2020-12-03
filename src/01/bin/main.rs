cd88c03fe9d6132520c78143e7a72c116bdca594
Victor Barua <victor.barua@gmail.com>
2020-12-02 20:11:38 -0800

    Day 1 Problem 2

--- src/01/bin/main.rs
+++ src/01/bin/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 
 const TARGET_VALUE: i32 = 2020;
 
-fn dual_sum(target: i32, input: &Vec<i32>) -> (i32, i32) {
+fn dual_sum(target: i32, input: &Vec<i32>) -> Option<(i32, i32)> {
   let mut forward_iter = input.iter();
   let mut reverse_iter = input.iter().rev();
 
@@ -15,7 +15,9 @@ fn dual_sum(target: i32, input: &Vec<i32>) -> (i32, i32) {
   loop {
     let sum = first + last;
     if sum == target {
-      return (*first, *last);
+      return Some((*first, *last));
+    } else if first == last {
+      return None
     } else if sum < target {
       first = forward_iter.next().unwrap();
     } else {
@@ -24,17 +26,41 @@ fn dual_sum(target: i32, input: &Vec<i32>) -> (i32, i32) {
   }
 }
 
+fn triple_sum(target: i32, input: &Vec<i32>) -> Option<(i32, i32, i32)> {
+  for x in input {
+     match dual_sum(target - x, input) {
+       Some(res) => {
+        let (y, z) = res;
+        return Some((*x, y, z));
+       },
+       None => continue
+     }
+  }
+  None
+}
+
 fn main() -> std::io::Result<()> {
-  let file = File::open("src/01/bin/test_input.txt")?;
+  let file = File::open("src/01/bin/input.txt")?;
   let mut buf_reader = BufReader::new(file);
   let mut contents = String::new();
   buf_reader.read_to_string(&mut contents)?;
   let mut lines  = contents.lines().map(|s| i32::from_str(s).unwrap()).collect::<Vec<_>>();
   lines.sort();
 
-  let (i, j) = dual_sum(TARGET_VALUE, &lines);
-  println!("Dual Sum Solution");
-  println!("{} + {} = {}", i, j, i + j);
-  println!("{} * {} = {}", i, j, i * j);
+  dual_sum(TARGET_VALUE, &lines).map(|res| {
+    let (i, j) = res;
+    println!("Dual Sum Solution");
+    println!("{} + {} = {}", i, j, i + j);
+    println!("{} * {} = {}", i, j, i * j);
+    println!();
+  });
+
+  triple_sum(TARGET_VALUE, &lines).map(|res| {
+    let (x, y, z) = res;
+    println!("Triple Sum Solution");
+    println!("{} + {} + {} = {}", x, y, z, x + y + z);
+    println!("{} * {} * {} = {}", x, y, z, x * y * z);
+  });
+
   Ok(())
 }
