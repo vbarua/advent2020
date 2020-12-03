2275958cbfe1e586ad6dcac4771a38cd64961597
Victor Barua <victor.barua@gmail.com>
2020-12-02 19:38:25 -0800

    BugFix: dual_sum was skipping values

--- src/01/bin/main.rs
+++ src/01/bin/main.rs
@@ -6,32 +6,35 @@ use std::io::prelude::*;
 
 const TARGET_VALUE: i32 = 2020;
 
+fn dual_sum(target: i32, input: &Vec<i32>) -> (i32, i32) {
+  let mut forward_iter = input.iter();
+  let mut reverse_iter = input.iter().rev();
+
+  let mut first = forward_iter.next().unwrap();
+  let mut last = reverse_iter.next().unwrap();
+  loop {
+    let sum = first + last;
+    if sum == target {
+      return (*first, *last);
+    } else if sum < target {
+      first = forward_iter.next().unwrap();
+    } else {
+      last = reverse_iter.next().unwrap();
+    }
+  }
+}
+
 fn main() -> std::io::Result<()> {
-  let file = File::open("src/01/bin/input.txt")?;
+  let file = File::open("src/01/bin/test_input.txt")?;
   let mut buf_reader = BufReader::new(file);
   let mut contents = String::new();
   buf_reader.read_to_string(&mut contents)?;
   let mut lines  = contents.lines().map(|s| i32::from_str(s).unwrap()).collect::<Vec<_>>();
   lines.sort();
-  let forward_iter = lines.iter();
-  let mut reverse_iter = lines.iter().rev();
-  // let last = reverse_iter.next();
-  for i in forward_iter {
-    while let Some(j) = reverse_iter.next() {
-      let sum = i + j;
-      if (sum) == TARGET_VALUE {
-        println!("{} + {} = {}\n{} * {} = {}",
-          i, j, sum, i, j, i*j);
-      } else if sum < TARGET_VALUE {
-        break;
-      }
-    }
-  }
-  // println!("{}", first);
 
-  // for l in lines.iter() {
-  //   println!("{}", l);
-  // }
-
+  let (i, j) = dual_sum(TARGET_VALUE, &lines);
+  println!("Dual Sum Solution");
+  println!("{} + {} = {}", i, j, i + j);
+  println!("{} * {} = {}", i, j, i * j);
   Ok(())
 }
