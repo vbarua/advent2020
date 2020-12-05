5d538e008d71120d6dac750215fd4b9c15ddcb98
Victor Barua <victor.barua@gmail.com>
2020-12-05 00:24:18 -0800

    Day 3 Part 2

--- src/03/bin/main.rs
+++ src/03/bin/main.rs
@@ -3,6 +3,27 @@ use std::io::BufReader;
 
 use std::io::prelude::*;
 
+fn traverse(
+  terrain: &Vec<Vec<char>>,
+  dx: usize, dy: usize
+) -> u64 {
+  let width = terrain.first().unwrap().len();
+  let height = terrain.len();
+
+  let mut x = 0;
+  let mut y = 0;
+  let mut collisions = 0;
+  while y < height - 1 {
+    x += dx;
+    x %= width;
+    y += dy;
+    let collision = terrain[y][x] == '#';
+    if collision { collisions += 1; }
+  }
+  println!("dx: {}, dy: {} - Collisions: {}", dx, dy, collisions);
+  return collisions;
+}
+#[cfg(debug_assertions)]
 fn main() -> std::io::Result<()> {
     let file = File::open("src/03/bin/input.txt")?;
   let mut buf_reader = BufReader::new(file);
@@ -10,20 +31,16 @@ fn main() -> std::io::Result<()> {
   buf_reader.read_to_string(&mut contents)?;
   let terrain: Vec<Vec<char>> = contents.lines().map(|l| l.chars().collect()).collect::<Vec<_>>();
 
-  let width = terrain.first().unwrap().len();
-  let height = terrain.len();
+  let results = [
+    traverse(&terrain, 1, 1),
+    traverse(&terrain, 3, 1),
+    traverse(&terrain, 5, 1),
+    traverse(&terrain, 7, 1),
+    traverse(&terrain, 1, 2)
+  ];
 
-  let mut x = 0;
-  let mut collisions = 0;
-  for y in 1..height {
-    x += 3;
-    x %= width;
-    let collision = terrain[y][x] == '#';
-    if collision {
-      collisions += 1;
-    }
-    println!("({}, {}, {})", x, y, collision);
-  }
-  println!("Number of Collisions: {}", collisions);
+  let trees_product: u64 = results.iter().product();
+  println!("Product of All Trees: {}", trees_product);
+
   Ok(())
 }
