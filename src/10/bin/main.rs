e928e22737bdc525c2fce5781b76952abcaa1c32
Victor Barua <victor.barua@gmail.com>
2020-12-11 18:53:01 -0800

    Day 10 Part 2 (Sleep + Memoization)

--- src/10/bin/main.rs
+++ src/10/bin/main.rs
@@ -1,21 +1,25 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 
 use std::io::prelude::*;
 
-fn combine(base: i32, target: i32, input: &[i32]) -> i32 {
-    if base == target {
-        return 1;
+fn paths(input: &[i32]) -> i64 {
+    let minimum_joltage = *input.iter().min().unwrap();
+    let maximum_joltage = *input.iter().max().unwrap();
+    let mut joltages: HashMap<i32, i64> = input.iter().cloned().map(|j| (j, 0)).collect();
+    joltages.insert(maximum_joltage, 1);
+
+    for &j in input.iter().rev().skip(1) {
+        let j1 = joltages.get(&(j + 1)).cloned().unwrap_or(0);
+        let j2 = joltages.get(&(j + 2)).cloned().unwrap_or(0);
+        let j3 = joltages.get(&(j + 3)).cloned().unwrap_or(0);
+
+        let paths_from_j = j1 + j2 + j3;
+        joltages.insert(j, paths_from_j);
     }
-    let mut combinations = 0;
-    for (index, joltage) in input.iter().enumerate() {
-        if *joltage - base > 3 {
-            break;
-        }
-        println!("{}, {}, {}, {}", *joltage, base, index, combinations);
-        combinations += combine(*joltage, target, &input[index + 1..]);
-    }
-    return combinations;
+
+    return *joltages.get(&minimum_joltage).unwrap();
 }
 
 fn main() -> std::io::Result<()> {
@@ -25,14 +29,12 @@ fn main() -> std::io::Result<()> {
     buf_reader.read_to_string(&mut contents)?;
 
     let mut input: Vec<i32> = contents.lines().map(|l| l.parse().unwrap()).collect();
-    let mut input2 = input.clone();
-
-    // Part 1
     let device_joltage = input.iter().max().unwrap() + 3;
     input.push(0);
     input.push(device_joltage);
     input.sort();
 
+    // Part 1
     let diffs: Vec<i32> = input
         .iter()
         .zip(input.iter().skip(1))
@@ -44,11 +46,23 @@ fn main() -> std::io::Result<()> {
     println!("Part 1: {} * {} = {}", ones, threes, ones * threes);
 
     // Part 2
-    input2.push(device_joltage);
-    input2.sort();
-
-    let combinations = combine(0, device_joltage, &input2);
+    let combinations = paths(&input);
     println!("Part 2: {} Combinations", combinations);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn paths_tests() {
+        assert_eq!(1, paths(&vec![0]));
+        assert_eq!(1, paths(&vec![0, 3]));
+        assert_eq!(1, paths(&vec![0, 3, 6]));
+        assert_eq!(4, paths(&vec![0, 1, 2, 3]));
+    }
 }
